53b41a7307b8db0c126c372de712c0a1d05e51ff
ducc <spielerc@gmail.com>
2023-11-15 02:33:43 -0700

    projector_test.scene now minimally reproduces the light cookie issue

---
+++ projector_components/code/Components/LightCookieCopierComponent.cs
@@ -0,0 +1,35 @@
+﻿namespace Sandbox;
+
+public class LightCookieCopierComponent : BaseComponent
+{
+	[Property] public SpotLightComponent FromSpotLight { get; set; }
+	[Property] public SpotLightComponent ToSpotLight { get; set; }
+	[Property] public bool ShouldCopyData { get; set; } = false;
+	[Property] public float DataCopyInterval { get; set; } = 1f;
+
+	private RealTimeSince _lastDataCopyTime { get; set; }
+
+	public override void Update()
+	{
+		// There's nothing to update.
+		if ( FromSpotLight.Cookie is null && ToSpotLight.Cookie is null )
+			return;
+
+		// ToSpotLight doesn't have a cookie, or it's the wrong size.
+		if ( ToSpotLight.Cookie is null || ToSpotLight.Cookie.Size != FromSpotLight.Cookie.Size )
+		{
+			Log.Info( $"({GameObject.Name}) Copying light cookie from ({FromSpotLight.GameObject.Name}) to ({ToSpotLight.GameObject.Name})" );
+			var fromSize = FromSpotLight.Cookie.Size;
+			ToSpotLight.Cookie = Texture.Create( (int)fromSize.x, (int)fromSize.y )
+				.WithUAVBinding()
+				.Finish();
+		}
+
+		if ( !ShouldCopyData || _lastDataCopyTime < DataCopyInterval )
+			return;
+
+		Log.Info( $"({GameObject.Name}) Copying light cookie texture data from ({FromSpotLight.GameObject.Name}) to ({ToSpotLight.GameObject.Name})" );
+		_lastDataCopyTime = 0f;
+		ToSpotLight.Cookie.Update( FromSpotLight.Cookie.GetPixels() );
+	}
+}
---
+++ projector_components/code/Components/LightCookieGeneratorComponent.cs
@@ -0,0 +1,37 @@
+﻿namespace Sandbox;
+
+public class LightCookieGeneratorComponent : BaseComponent
+{
+	[Property] public SpotLightComponent SpotLight { get; set; }
+	/// <summary>
+	/// Amount of time in seconds between each cookie generation.
+	/// </summary>
+	[Property] public float GenerationInterval { get; set; } = 1f;
+	[Property] public Vector2 CookieSize { get; set; } = new Vector2( 64 );
+	private RealTimeSince _lastGenerationTime { get; set; }
+
+	public override void Update()
+	{
+		base.Update();
+
+		if (_lastGenerationTime > GenerationInterval)
+		{
+			_lastGenerationTime = 0f;
+			SpotLight.Cookie = GenerateCookie();
+		}
+	}
+
+	private Texture GenerateCookie()
+	{
+		var cookie = Texture.Create( (int)CookieSize.x, (int)CookieSize.y )
+			.WithUAVBinding()
+			.Finish();
+		var data = new Color32[(int)CookieSize.x * (int)CookieSize.y];
+		for(int i = 0; i < data.Length; i++ )
+		{
+			data[i] = Color.Random;
+		}
+		cookie.Update( data );
+		return cookie;
+	}
+}
